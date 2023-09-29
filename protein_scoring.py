98747150ecb1baa3fd7318b11923c3ef8ff07445
jeremietd <jeremie.darmawan@student.i3l.ac.id>
2023-09-29 03:51:26 +0100

    minor edits

--- protein_scoring.py
+++ protein_scoring.py
@@ -110,7 +110,7 @@ sub_gap_open = args.sub_gap_open
 sub_gap_extend = args.sub_gap_extend
 
 rand_id = randint(10000, 99999) # Necessary for parallelization
-print("===========================================")
+# print("===========================================")
 print(f"Using random ID {rand_id} for temporary files")
 
 #concatenate reference sequences
